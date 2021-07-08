f51403a4af0b1da20a32297204ce08004ed69a2e
Leandro Correia <leandro.correia@gmail.com>
2021-07-08 17:04:04 -0300

    Exclusao da pasta datasets

--- carga_dados.py
+++ carga_dados.py
@@ -3,16 +3,21 @@ import logging
 
 class CargaDados:
 
-    df_seg = []
+    dados_seg = []
+    path_dados_seg=None
+    cabecalho_dados_seg=None
 
-    def __init__(self):
+    def __init__(self, path_dados_seg='./extracao-segmentos-atos.csv',
+                    cabecalho_dados_seg=['id_ato', 'data_pub', 'tipo_ato', 
+                                        'id_seg', 'tipo_seg', 'txt_seg']):
         logging.basicConfig(level=logging.INFO)
+        self.path_dados_seg = path_dados_seg
+        self.cabecalho_dados_seg = cabecalho_dados_seg
     
     def executar(self):
         logging.info('Carregando dados de segmentos...')
-        colunas = ['id_ato', 'data_pub', 'tipo_ato', 'id_seg', 'tipo_seg', 'txt_seg']
-        self.df_seg = pd.read_csv('./datasets/extracao-segmentos-atos.csv', 
-                            delimiter='|', names=colunas, quotechar="'")
+        self.df_seg = pd.read_csv(self.path_dados_seg, delimiter='|', 
+                                    names=self.cabecalho_dados_seg, quotechar="'")
         logging.info('{0} registros carregados.'.format(self.df_seg['id_seg'].count()))
 
         return self.df_seg
