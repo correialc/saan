3bd35cdd0c036457b8cf78ac1bcf5ef21d688be6
Leandro Correia <leandro.correia@serpro.gov.br>
2021-07-09 19:15:20 -0300

    Remocao de tags HTML

--- limpeza_dados.py
+++ limpeza_dados.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import logging
+import re
+from html.parser import HTMLParser
 
 class LimpezaDados:
 
@@ -8,7 +10,14 @@ class LimpezaDados:
     def __init__(self, df_seg):
         logging.basicConfig(level=logging.INFO)
         self.df_seg = df_seg
+
+    def remover_tags_html(self):
+        logging.info('Removendo tags HTML...')
+        padrao_tag_html = re.compile('<.*?>')
+        remover_tags = lambda txt : re.sub(padrao_tag_html, '', str(txt)) 
+        self.df_seg['txt_seg'] = self.df_seg['txt_seg'].apply(remover_tags)
     
     def executar(self):
-        logging.info('Etapa limpeza dados...')
+        self.remover_tags_html()
+        logging.info('Limpeza de dados concluída.')
         return self.df_seg
