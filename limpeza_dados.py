819c98da1420ee59efada167e2af0cf2ffb00635
Leandro Correia <leandro.correia@serpro.gov.br>
2021-07-13 21:40:13 -0300

    Exclusao de segmentos de atos nao ADE

--- limpeza_dados.py
+++ limpeza_dados.py
@@ -11,6 +11,14 @@ class LimpezaDados:
         logging.basicConfig(level=logging.INFO)
         self.df_seg = df_seg
 
+    def remover_segmentos_nao_ade(self):
+        logging.info('Excluindo segmentos dos atos que não são ADE...')
+        qtd_seg = self.df_seg['id_seg'].count()
+        self.df_seg = self.df_seg[self.df_seg['tipo_ato']=='ADE'].copy()
+        qtd_seg_ade = self.df_seg['id_seg'].count()
+        logging.info(f'{qtd_seg - qtd_seg_ade} segmentos de atos não ADE excluídos.')
+        logging.info(f'Restaram {qtd_seg_ade} segmentos de atos ADE.')
+    
     def remover_tags_html(self):
         logging.info('Removendo tags HTML...')
         padrao_tag_html = re.compile('<.*?>')
@@ -18,11 +26,12 @@ class LimpezaDados:
         self.df_seg['txt_seg'] = self.df_seg['txt_seg'].apply(remover_tags)
     
     def remover_escape_html(self):
-        logging.info('Removendo caracteres de escape HTML')
+        logging.info('Removendo caracteres de escape HTML...')
         remover_escape_chars = lambda txt : HTMLParser().unescape(txt)
         self.df_seg['txt_seg'] = self.df_seg['txt_seg'].apply(remover_escape_chars)
 
     def executar(self):
+        self.remover_segmentos_nao_ade()
         self.remover_tags_html()
         self.remover_escape_html()
         logging.info('Limpeza de dados concluída.')
