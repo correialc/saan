592407cbba0f47ceb7ee6401e8dec00410722e0b
Leandro Correia <leandro.correia@serpro.gov.br>
2021-07-19 11:00:07 -0300

    Reclassificacao de nao informados para artigos

--- limpeza_dados.py
+++ limpeza_dados.py
@@ -47,11 +47,21 @@ class LimpezaDados:
         remover_escape_chars = lambda txt : HTMLParser().unescape(txt)
         self.df_seg['txt_seg'] = self.df_seg['txt_seg'].apply(remover_escape_chars)
 
+    def reclassificar_nao_identificados(self):
+        logging.info('Reclassificando segmentos não identificados...')
+        df_seg_ni = self.df_seg[self.df_seg['tipo_seg'] == 'Não Identificado']
+        padrao_artigo = re.compile('^\s{0,}Art[\s\.]')
+        qtd_seg_reclassificaveis = df_seg_ni[df_seg_ni['txt_seg'].str.contains(padrao_artigo)].count()['id_seg']
+        ids_reclassificaveis = df_seg_ni[df_seg_ni['txt_seg'].str.contains(padrao_artigo)]['id_seg']
+        self.df_seg.loc[self.df_seg['id_seg'].isin(ids_reclassificaveis), 'tipo_seg'] = 'Artigo'
+        logging.info(f'{qtd_seg_reclassificaveis} segmentos reclassificados como artigos.')
+
     def executar(self):
         self.remover_segmentos_nao_ade()
         self.remover_segmentos_nao_representativos()
         self.remover_segmentos_nulos()
         self.remover_tags_html()
         self.remover_escape_html()
+        self.reclassificar_nao_identificados()
         logging.info('Limpeza de dados concluída.')
         return self.df_seg
