e4aa97eff5810c3da663b7488edb2ac78ab63867
Leandro Correia <leandro.correia@serpro.gov.br>
2021-08-07 20:43:14 -0300

    Substituicao da tokenizacao do Spacy pela do NLTK

--- preprocessamento.py
+++ preprocessamento.py
@@ -1,30 +1,15 @@
 import logging
-import spacy
 import nltk
+from nltk.tokenize import WhitespaceTokenizer, RegexpTokenizer
 from string import punctuation
 
 class Preprocessamento:
     
     def __init__(self):
         logging.basicConfig(level=logging.INFO)
-        logging.info("Carregando modelo de Português para preprocessamento...")
-        self.nlp = spacy.load('pt_core_news_sm')
         nltk.download('stopwords')
         self.stopwords = nltk.corpus.stopwords.words('portuguese')
-                
-    def executar_pipeline_preprocessamento(self, dados):
-        logging.info("Executando pipeline de preprocessamento...")
-        with self.nlp.select_pipes(enable=['tagger','lemmatizer']):
-            dados.prep = dados.limp
-            dados.prep['docs'] = list(self.nlp.pipe(dados.limp['txt_seg']))
-            logging.info("Salvando resultado da tokenização...")
-            dados.prep['tokens'] = dados.prep['docs'].apply(self.salvar_tokens)
-
-    def salvar_tokens(self, doc):
-        termos = []
-        for token in doc:
-            termos.append(token.text)
-        return termos
+        self.tokenizer = RegexpTokenizer(r'\w+')
     
     def converter_para_minusculo(self, tokens):
         minusculo = map(str.lower, tokens)
@@ -39,12 +24,15 @@ class Preprocessamento:
         return list(sem_stop_words)
 
     def executar(self, dados):
-        self.executar_pipeline_preprocessamento(dados)
+        dados.prep = dados.limp.copy()
+        logging.info('Realizando tokenização...')
+        dados.prep['tokens'] = dados.prep['txt_seg'].apply(self.tokenizer.tokenize)
         logging.info('Convertendo caracteres para minúsculo...')
         dados.prep['tokens'] = dados.prep['tokens'].apply(self.converter_para_minusculo)
         logging.info('Removendo pontuação...')
         dados.prep['tokens'] = dados.prep['tokens'].apply(self.remover_pontuacao)
         logging.info('Removendo stopwords...')
         dados.prep['tokens'] = dados.prep['tokens'].apply(self.remover_stopwords)
+        logging.info('Preprocessamento concluído.')
 
         
