3587386273f487e12e0f4a569b4d861467698a13
Leandro Correia <leandro.correia@serpro.gov.br>
2021-08-07 16:59:39 -0300

    Conversao para minusculo

--- preprocessamento.py
+++ preprocessamento.py
@@ -13,7 +13,13 @@ class Preprocessamento:
         with self.nlp.select_pipes(enable=['tagger','lemmatizer']):
             dados.seg_prep = dados.seg_limp
             dados.seg_prep['docs'] = list(self.nlp.pipe(dados.seg_limp['txt_seg']))
-            
+
+    def converter_para_minusculo(self, dados):
+        logging.info("Convertendo texto para minúsculo...")
+        minusculo = [lambda txt : txt.lower()]
+        dados.seg_prep['txt_seg'] = dados.seg_prep['txt_seg'].apply(minusculo)
+
     def executar(self, dados):
         self.executarPipelinePreprocessamento(dados)
+        self.converter_para_minusculo(dados)
         
