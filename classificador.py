73eb5de18ba32204b306d25ee1795d37b1732e8c
Leandro Correia <leandro.correia@serpro.gov.br>
2021-08-09 21:14:29 -0300

    FIX: Registro das metricas

--- classificador.py
+++ classificador.py
@@ -12,7 +12,9 @@ class Classificador:
     def calcular_metricas(self, dados):
         y_predito = self.estimador.predict(dados.Xte)
 
-        dados.metricas[self.nome]['precision'] = round(precision_score(dados.Yte, y_predito, pos_label=1, average='macro'),4)
-        dados.metricas[self.nome]['recall'] = round(recall_score(dados.Yte, y_predito, pos_label=1, average='macro'), 4)
-        dados.metricas[self.nome]['f1'] = round(f1_score(dados.Yte, y_predito, pos_label=1, average='macro'),4)
-        dados.metricas[self.nome]['accuracy'] = round(accuracy_score(dados.Yte, y_predito),4)
+        dados.metricas[self.nome] = {
+            'acuracia': round(accuracy_score(dados.Yte, y_predito),4),
+            'precisao': round(precision_score(dados.Yte, y_predito, pos_label=1, average='macro'),4),
+            'revocacao': round(recall_score(dados.Yte, y_predito, pos_label=1, average='macro'), 4),
+            'f1': round(f1_score(dados.Yte, y_predito, pos_label=1, average='macro'),4)
+        }
