b10949bcae5ac374b2a94da636cd5dc12cfb4e6a
Leandro Correia <leandro.correia@serpro.gov.br>
2021-08-09 21:51:49 -0300

    Reorganizacao das pastas do projeto

--- fluxo/carga_dados.py
+++ fluxo/carga_dados.py
@@ -6,7 +6,7 @@ class CargaDados:
     path_dados_seg=None
     cabecalho_dados_seg=None
 
-    def __init__(self, path_dados_seg='./extracao-segmentos-atos.csv',
+    def __init__(self, path_dados_seg='./dados/extracao-segmentos-atos.csv',
                     cabecalho_dados_seg=['id_ato', 'data_pub', 'tipo_ato', 
                                         'id_seg', 'tipo_seg', 'txt_seg']):
         logging.basicConfig(level=logging.INFO)
