37cfe6f7bf499097fb6ed2a06b302a689a338b95
Leandro Correia <leandro.correia@serpro.gov.br>
2021-08-30 18:33:29 -0300

    Generalizacao da limpeza para PORT e SC

--- fluxo/limpeza_dados.py
+++ fluxo/limpeza_dados.py
@@ -5,9 +5,25 @@ from html.parser import HTMLParser
 
 class LimpezaDados:
 
+    tipos_seg_por_tipo_ato = {
+        'ADE': ['Artigo', 'Ementa', 'Fecho', 'Inciso', 'Não Identificado'],
+        'Port.': ['Alínea', 'Artigo', 'Ementa', 'Fecho', 'Inciso', 'Não Identificado', 'Parágrafo'],
+        'SC': ['Ementa', 'Fecho', 'Não Identificado']
+    }
+
+
     def __init__(self, dados):
         logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s", datefmt='%H:%M:%S')
+
+    def carregar_dados_originais(self, dados):
+        logging.info('(Re)Carregando segmentos originais...')
         dados.limp = dados.orig.copy()
+
+    def remover_segmentos_tipo_anexo(self, dados):
+        logging.info('Removendo todos os segmentos do tipo Anexo...')
+        qtd_seg_anexo = dados.limp[dados.limp['tipo_seg']=='Anexo']['id_seg'].count()
+        dados.limp = dados.limp[dados.limp['tipo_seg']!='Anexo'].copy()
+        logging.info(f'{qtd_seg_anexo} segmentos do tipo Anexo excluídos.')
 
     def remover_segmentos_filtro_tipo_ato(self, dados, tipo_ato):
         logging.info(f'Excluindo segmentos dos atos que não são {tipo_ato}...')
@@ -18,9 +34,9 @@ class LimpezaDados:
         logging.info(f'Restaram {qtd_seg_filtrados} segmentos de atos {tipo_ato}.')
         
     
-    def remover_segmentos_nao_representativos(self, dados):
-        logging.info('Removendo segmentos não representativos...')
-        tipos_seg_representativos = ['Artigo', 'Não Identificado', 'Ementa', 'Fecho', 'Inciso']
+    def remover_segmentos_nao_representativos(self, dados, tipo_ato):
+        logging.info(f'Removendo segmentos não representativos para o tipo {tipo_ato}...')
+        tipos_seg_representativos = self.tipos_seg_por_tipo_ato[tipo_ato]
         qtd_seg = dados.limp['id_seg'].count()
         qtd_seg_rep = dados.limp['tipo_seg'].isin(tipos_seg_representativos).sum()
         dados.limp = dados.limp[dados.limp['tipo_seg'].isin(tipos_seg_representativos)]
@@ -61,9 +77,11 @@ class LimpezaDados:
         
 
     def executar(self, dados, tipo_ato):
-        self.remover_segmentos_filtro_tipo_ato(dados, tipo_ato)
-        self.remover_segmentos_nao_representativos(dados)
+        self.carregar_dados_originais(dados)
         self.remover_segmentos_nulos(dados)
+        self.remover_segmentos_tipo_anexo(dados)
+        self.remover_segmentos_filtro_tipo_ato(dados, tipo_ato)
+        self.remover_segmentos_nao_representativos(dados, tipo_ato)
         self.remover_tags_html(dados)
         self.remover_escape_html(dados)
         #self.reclassificar_nao_identificados(dados)
