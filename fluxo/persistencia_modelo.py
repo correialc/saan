e437f3d5211ffd565c13a1817d8732c73eb63a6c
Leandro Correia <leandro.correia@serpro.gov.br>
2021-11-08 12:17:58 -0300

    Persistencia de vetorizadores

--- fluxo/persistencia_modelo.py
+++ fluxo/persistencia_modelo.py
@@ -15,3 +15,25 @@ class PersistenciaModelo:
         caminho_arquivo_modelo = (self.caminho_modelos+'classificador-segmentos-'+tipo_ato+'.mdl')
         joblib.dump(modelo, caminho_arquivo_modelo)
         logging.info(f'Modelo salvo em {caminho_arquivo_modelo}.')
+
+
+    def carregar_modelo(self, tipo_ato):
+        logging.info(f'Carregando modelo treinado para o tipo {tipo_ato}...')
+        tipo_ato = ''.join([caractere.lower() for caractere in tipo_ato if caractere not in punctuation])
+        caminho_arquivo_modelo = (self.caminho_modelos+'classificador-segmentos-'+tipo_ato+'.mdl')
+        return joblib.load(caminho_arquivo_modelo)
+
+
+    def salvar_vetorizador(self, vetorizador, tipo_ato):
+        logging.info(f'Salvando vetorizador para o tipo {tipo_ato}...')
+        tipo_ato = ''.join([caractere.lower() for caractere in tipo_ato if caractere not in punctuation])
+        caminho_arquivo_modelo = (self.caminho_modelos+'vetorizador-'+tipo_ato+'.mdl')
+        joblib.dump(vetorizador, caminho_arquivo_modelo)
+        logging.info(f'Vetorizador salvo em {caminho_arquivo_modelo}.')
+
+    
+    def carregar_vetorizador(self, tipo_ato):
+        logging.info(f'Carregando vetorizador treinado para o tipo {tipo_ato}...')
+        tipo_ato = ''.join([caractere.lower() for caractere in tipo_ato if caractere not in punctuation])
+        caminho_arquivo_modelo = (self.caminho_modelos+'vetorizador-'+tipo_ato+'.mdl')
+        return joblib.load(caminho_arquivo_modelo)
