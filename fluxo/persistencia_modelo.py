e34b9e7113775381cfdc3285b17806f670362b6d
Leandro Correia <leandro.correia@serpro.gov.br>
2021-11-08 10:56:45 -0300

    Generalizacao da persistencia de modelos

--- fluxo/persistencia_modelo.py
+++ fluxo/persistencia_modelo.py
@@ -4,14 +4,14 @@ from string import punctuation
 
 class PersistenciaModelo:
 
-    def __init__(self, tipo_ato):
+    def __init__(self):
         logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s", datefmt='%H:%M:%S')
-        self.tipo_ato = ''.join([caractere for caractere in tipo_ato if caractere not in punctuation])
         self.caminho_modelos = './modelos/'
 
 
-    def salvar_modelo(self, modelo):
-        logging.info(f'Salvando modelo treinado para o tipo {self.tipo_ato}...')
-        caminho_arquivo_modelo = (self.caminho_modelos+'classificador-segmentos-'+self.tipo_ato+'.mdl').lower()
+    def salvar_modelo(self, modelo, tipo_ato):
+        logging.info(f'Salvando modelo treinado para o tipo {tipo_ato}...')
+        tipo_ato = ''.join([caractere.lower() for caractere in tipo_ato if caractere not in punctuation])
+        caminho_arquivo_modelo = (self.caminho_modelos+'classificador-segmentos-'+tipo_ato+'.mdl')
         joblib.dump(modelo, caminho_arquivo_modelo)
         logging.info(f'Modelo salvo em {caminho_arquivo_modelo}.')
