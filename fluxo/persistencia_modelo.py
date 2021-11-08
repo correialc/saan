a7abb4d49c0fefb6f46d9701286de523a04d5b7f
Leandro Correia <leandro.correia@serpro.gov.br>
2021-11-08 10:33:26 -0300

    Tratando nome do modelo durante a persistencia

--- fluxo/persistencia_modelo.py
+++ fluxo/persistencia_modelo.py
@@ -1,12 +1,14 @@
 import logging
 import joblib
+from string import punctuation
 
 class PersistenciaModelo:
 
     def __init__(self, tipo_ato):
         logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s", datefmt='%H:%M:%S')
-        self.tipo_ato = tipo_ato
+        self.tipo_ato = ''.join([caractere for caractere in tipo_ato if caractere not in punctuation])
         self.caminho_modelos = './modelos/'
+
 
     def salvar_modelo(self, modelo):
         logging.info(f'Salvando modelo treinado para o tipo {self.tipo_ato}...')
