704ed2a9e0ab1703d3b07c42a2a8f71d60f95787
Leandro Correia <leandro.correia@serpro.gov.br>
2021-11-17 11:07:18 -0300

    Reestruturacao do modulos da aplicacao principal

--- app/classificador_app.py
+++ app/classificador_app.py
@@ -1,4 +1,8 @@
+import os
 import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pandas as pd
 
 from fluxo.dados import Dados
--- fluxo/classificador_segmentos.py
+++ fluxo/classificador_segmentos.py
@@ -1,4 +1,8 @@
+import os
 import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import logging
 import pandas as pd
 from string import punctuation
